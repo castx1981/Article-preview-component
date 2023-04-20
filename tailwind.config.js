1a87397dc5baa4c77c3d1eaee3a77a1b1a00023c
Xiaoliang Luan <xluan@qjnext.com>
2023-04-21 00:26:52 +0800

    mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,20 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Manrope', 'sans-serif'],
+      },
+      fontSize: {
+        'body': '0.8125rem',
+      },
+      colors: {
+        'very-dark-grayish-blue': 'hsl(217, 19%, 35%)',
+        'desaturated-dark-blue': 'hsl(214, 17%, 51%)',
+        'grayish-blue': 'hsl(212, 23%, 69%)',
+        'light-grayish-blue': 'hsl(210, 46%, 95%)',
+      },
+    },
   },
   plugins: [],
 }
-
